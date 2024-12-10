10588cceec4598c1024ef6381d3cef04e7119905
lucas carli <lucas.carli@tecso.coop>
2024-12-10 11:34:03 -0300

    feat: third commit

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,57 +1,34 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
+import { AddCategory, GifGrid } from "./components";
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+    const [categories, setCategories] = useState(['One Punch']);
 
-    const onAddCategory = ( newCategory) => {
-    setCategories([ newCategory, ...categories])
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) return
+        setCategories([newCategory, ...categories])
     }
 
 
     return (
         <>
-            {/* titulo */}
             <h1>GifExpertApp</h1>
 
-            {/* input */}
-
             <AddCategory
-                // setCategories= {setCategories}
-                onNewCategory={ (value) => onAddCategory(value)}// Porque esta emitiendo algo
+                onNewCategory={(value) => onAddCategory(value)}
             />
 
-            {/* listado de gif */}
-            <ol>
-                {categories.map(category => {
-                    return <li key={category}>{category}</li>
-                })}
-            </ol>
-            {/* gif item */}
+            {
+                categories.map((category) => (
+                    <GifGrid
+                        key={category}
+                        category={category} />
+                ))
+            }
 
         </>
     )
 }
 
-// export const CounterApp = ({ value }) => {
 
-//     const [counter, setCounter] = useState(value);
-
-//     // Cuando se cambia el estado el componente se vuelve a renderizar
-
-//     const handleAdd = () => { setCounter( counter + 1 );}
-//     const handleSubstract = () => { setCounter( counter - 1 );}
-//     const handleReset = () => { setCounter( value );}
-
-//     return (
-//         <>
-//             <div>CounterApp</div>
-//             <h2> {counter} </h2>
-
-//             <button onClick={handleAdd}> +1 </button>
-//             <button onClick={handleSubstract}> -1 </button>
-//             <button aria-label="btn-reset" onClick={handleReset}> Reset </button>
-//         </>
-//     );
-// }
